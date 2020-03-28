11d0bfa8f34349f487aff880aa2d22ac22d336f8
tharunshiv <tharun.rocky14@gmail.com>
2020-03-28 13:08:48 +0530

    uncommented -1

--- image_diff.py
+++ image_diff.py
@@ -1,204 +1,204 @@
-# # converting video to images
-# import cv2; print(cv2.__version__)
-# import numpy as np
+# converting video to images
+import cv2; print(cv2.__version__)
+import numpy as np
 
-# '''----------------------
-# Converting first video to frames
-# --------------------------'''
+'''----------------------
+Converting first video to frames
+--------------------------'''
 
-# vidcap = cv2.VideoCapture('/home/tharunshiv/image-difference/virus.mp4')
-# def getFrame(sec, count):
-#     vidcap.set(cv2.CAP_PROP_POS_MSEC,sec*1000)
-#     hasFrames,image = vidcap.read()
-#     if hasFrames:
-#         cv2.imwrite("/home/tharunshiv/image-difference/one/"+str(count)+".jpg", image)	
-#     return hasFrames
+vidcap = cv2.VideoCapture('/home/tharunshiv/image-difference/virus.mp4')
+def getFrame(sec, count):
+    vidcap.set(cv2.CAP_PROP_POS_MSEC,sec*1000)
+    hasFrames,image = vidcap.read()
+    if hasFrames:
+        cv2.imwrite("/home/tharunshiv/image-difference/one/"+str(count)+".jpg", image)	
+    return hasFrames
 
-# count = 0
-# sec = 0
-# frameRate = 0.05 # it will capture image in each 0.5 second
-# success = getFrame(sec, count)
+count = 0
+sec = 0
+frameRate = 0.05 # it will capture image in each 0.5 second
+success = getFrame(sec, count)
 
-# while success:
-# 	count = count + 1
-# 	sec = sec + frameRate
-# 	sec = round(sec, 2)
-# 	success = getFrame(sec, count)
+while success:
+	count = count + 1
+	sec = sec + frameRate
+	sec = round(sec, 2)
+	success = getFrame(sec, count)
 
-# print("Converted First video into Frames")
-# print("We got ", count, " frames")
+print("Converted First video into Frames")
+print("We got ", count, " frames")
 
-# '''----------------------
-# Converting second video to frames
-# --------------------------'''
-# vidcap = cv2.VideoCapture('/home/tharunshiv/image-difference/virusMod.mp4')
-# def getFrame(sec, count):
-#     vidcap.set(cv2.CAP_PROP_POS_MSEC,sec*1000)
-#     hasFrames,image = vidcap.read()
-#     if hasFrames:
-#         cv2.imwrite("/home/tharunshiv/image-difference/two/"+str(count)+"_2.jpg", image)     # save frame as JPG file
-#     return hasFrames
+'''----------------------
+Converting second video to frames
+--------------------------'''
+vidcap = cv2.VideoCapture('/home/tharunshiv/image-difference/virusMod.mp4')
+def getFrame(sec, count):
+    vidcap.set(cv2.CAP_PROP_POS_MSEC,sec*1000)
+    hasFrames,image = vidcap.read()
+    if hasFrames:
+        cv2.imwrite("/home/tharunshiv/image-difference/two/"+str(count)+"_2.jpg", image)     # save frame as JPG file
+    return hasFrames
 
-# count = 0
-# sec = 0
-# frameRate = 0.05 # it will capture image in each 0.5 second
-# success = getFrame(sec, count)
+count = 0
+sec = 0
+frameRate = 0.05 # it will capture image in each 0.5 second
+success = getFrame(sec, count)
 
-# while success:
-# 	count = count+1
-# 	sec = sec+frameRate
-# 	sec = round(sec, 2)
-# 	success = getFrame(sec, count)
+while success:
+	count = count+1
+	sec = sec+frameRate
+	sec = round(sec, 2)
+	success = getFrame(sec, count)
 
-# count = int(count)
-# print("\nConverted Second video into Frames")
-# print("We got ", count, " frames\n")
+count = int(count)
+print("\nConverted Second video into Frames")
+print("We got ", count, " frames\n")
 
-# print("Now finding the difference thresholding between them\n")
+print("Now finding the difference thresholding between them\n")
 
-# '''----------------------
-# Finding the Diff
-# --------------------------'''
-# # import the necessary packages
-# from skimage.measure import compare_ssim
-# import argparse
-# import imutils
-# import cv2
+'''----------------------
+Finding the Diff
+--------------------------'''
+# import the necessary packages
+from skimage.measure import compare_ssim
+import argparse
+import imutils
+import cv2
 
-# for i in range(count):
-# 	# load the two input images
-# 	frameBasePath = '/home/tharunshiv/image-difference/one/'
-# 	imageA = cv2.imread(frameBasePath+str(i)+'.jpg')
-# 	frameBasePath2 = '/home/tharunshiv/image-difference/two/'
-# 	imageB = cv2.imread(frameBasePath2+str(i)+'_2.jpg')
+for i in range(count):
+	# load the two input images
+	frameBasePath = '/home/tharunshiv/image-difference/one/'
+	imageA = cv2.imread(frameBasePath+str(i)+'.jpg')
+	frameBasePath2 = '/home/tharunshiv/image-difference/two/'
+	imageB = cv2.imread(frameBasePath2+str(i)+'_2.jpg')
 
-# 	print(frameBasePath+str(i)+'.jpg')
-# 	print(frameBasePath2+str(i)+'_2.jpg')
-# 	# convert the images to grayscale
-# 	grayA = cv2.cvtColor(imageA, cv2.COLOR_BGR2GRAY)
-# 	grayB = cv2.cvtColor(imageB, cv2.COLOR_BGR2GRAY)
+	print(frameBasePath+str(i)+'.jpg')
+	print(frameBasePath2+str(i)+'_2.jpg')
+	# convert the images to grayscale
+	grayA = cv2.cvtColor(imageA, cv2.COLOR_BGR2GRAY)
+	grayB = cv2.cvtColor(imageB, cv2.COLOR_BGR2GRAY)
 
-# 	# kernel = np.ones((5, 5), np.float32)/32
-# 	# grayA = cv2.filter2D(grayA, -1, kernel)
+	# kernel = np.ones((5, 5), np.float32)/32
+	# grayA = cv2.filter2D(grayA, -1, kernel)
 
-# 	# kernel = np.ones((5, 5), np.float32)/32
-# 	# grayB = cv2.filter2D(grayB, -1, kernel)
+	# kernel = np.ones((5, 5), np.float32)/32
+	# grayB = cv2.filter2D(grayB, -1, kernel)
 
-# 	grayA = cv2.bilateralFilter(grayA,9,75,75)
-# 	grayB = cv2.bilateralFilter(grayB,9,75,75)
+	grayA = cv2.bilateralFilter(grayA,9,75,75)
+	grayB = cv2.bilateralFilter(grayB,9,75,75)
 	
 
-# 	# compute the Structural Similarity Index (SSIM) between the two
-# 	# images, ensuring that the difference image is returned
-# 	(score, diff) = compare_ssim(grayA, grayB, full=True)
-# 	diff = (diff * 255).astype("uint8")
-# 	# print("SSIM: {}".format(score))
+	# compute the Structural Similarity Index (SSIM) between the two
+	# images, ensuring that the difference image is returned
+	(score, diff) = compare_ssim(grayA, grayB, full=True)
+	diff = (diff * 255).astype("uint8")
+	# print("SSIM: {}".format(score))
 
-# 	# threshold the difference image, followed by finding contours to
-# 	# obtain the regions of the two input images that differ
-# 	thresh = cv2.threshold(diff, 0, 255,cv2.THRESH_BINARY_INV | cv2.THRESH_OTSU)[1]
+	# threshold the difference image, followed by finding contours to
+	# obtain the regions of the two input images that differ
+	thresh = cv2.threshold(diff, 0, 255,cv2.THRESH_BINARY_INV | cv2.THRESH_OTSU)[1]
 	
 
-# 	cv2.imwrite("/home/tharunshiv/image-difference/three/"+str(i)+"_diff.jpg", thresh) 
+	cv2.imwrite("/home/tharunshiv/image-difference/three/"+str(i)+"_diff.jpg", thresh) 
 
 
-# 	# cnts = cv2.findContours(thresh.copy(), cv2.RETR_EXTERNAL,
-# 	# 	cv2.CHAIN_APPROX_SIMPLE)
-# 	# cnts = imutils.grab_contours(cnts)
+	# cnts = cv2.findContours(thresh.copy(), cv2.RETR_EXTERNAL,
+	# 	cv2.CHAIN_APPROX_SIMPLE)
+	# cnts = imutils.grab_contours(cnts)
 
-# 	# # loop over the contours
-# 	# for c in cnts:
-# 	# 	# compute the bounding box of the contour and then draw the
-# 	# 	# bounding box on both input images to represent where the two
-# 	# 	# images differ
-# 	# 	(x, y, w, h) = cv2.boundingRect(c)
-# 	# 	cv2.rectangle(imageA, (x, y), (x + w, y + h), (0, 0, 255), 2)
-# 	# 	cv2.rectangle(imageB, (x, y), (x + w, y + h), (0, 0, 255), 2)
+	# # loop over the contours
+	# for c in cnts:
+	# 	# compute the bounding box of the contour and then draw the
+	# 	# bounding box on both input images to represent where the two
+	# 	# images differ
+	# 	(x, y, w, h) = cv2.boundingRect(c)
+	# 	cv2.rectangle(imageA, (x, y), (x + w, y + h), (0, 0, 255), 2)
+	# 	cv2.rectangle(imageB, (x, y), (x + w, y + h), (0, 0, 255), 2)
 
-# 	# show the output images
-# 	# cv2.imshow("Original", imageA)
-# 	# cv2.imshow("Modified", imageB)
-# 	# cv2.imshow("Diff", diff)
-# 	# cv2.imshow("Thresh", thresh)
-# 	# cv2.waitKey(0)
+	# show the output images
+	# cv2.imshow("Original", imageA)
+	# cv2.imshow("Modified", imageB)
+	# cv2.imshow("Diff", diff)
+	# cv2.imshow("Thresh", thresh)
+	# cv2.waitKey(0)
 
-# print("Found the thresholded diff between then and saved")
+print("Found the thresholded diff between then and saved")
 
-# # USAGE
-# # python image_diff.py --first images/original_01.png --second images/modified_01.png
+# USAGE
+# python image_diff.py --first images/original_01.png --second images/modified_01.png
 
-# # import the necessary packages
-# from skimage.measure import compare_ssim
-# import argparse
-# import imutils
-# import cv2
+# import the necessary packages
+from skimage.measure import compare_ssim
+import argparse
+import imutils
+import cv2
 
-# import numpy as np
+import numpy as np
 
-# # # construct the argument parse and parse the arguments
-# # ap = argparse.ArgumentParser()
-# # ap.add_argument("-f", "--first", required=True,
-# # 	help="first input image")
-# # ap.add_argument("-s", "--second", required=True,
-# # 	help="second")
-# # args = vars(ap.parse_args())
+# # construct the argument parse and parse the arguments
+# ap = argparse.ArgumentParser()
+# ap.add_argument("-f", "--first", required=True,
+# 	help="first input image")
+# ap.add_argument("-s", "--second", required=True,
+# 	help="second")
+# args = vars(ap.parse_args())
 
-# # # load the two input images
-# # imageA = cv2.imread(args["first"])
-# # imageB = cv2.imread(args["second"])
+# # load the two input images
+# imageA = cv2.imread(args["first"])
+# imageB = cv2.imread(args["second"])
 
 
-# for i in range(count):
-# 	frameBasePath = '/home/tharunshiv/image-difference/one/'
-# 	imageA = cv2.imread(frameBasePath+str(i)+'.jpg')
-# 	frameBasePath2 = '/home/tharunshiv/image-difference/two/'
-# 	imageB = cv2.imread(frameBasePath2+str(i)+'_2.jpg')
+for i in range(count):
+	frameBasePath = '/home/tharunshiv/image-difference/one/'
+	imageA = cv2.imread(frameBasePath+str(i)+'.jpg')
+	frameBasePath2 = '/home/tharunshiv/image-difference/two/'
+	imageB = cv2.imread(frameBasePath2+str(i)+'_2.jpg')
 
-# 	# convert the images to grayscale
-# 	grayA = cv2.cvtColor(imageA, cv2.COLOR_BGR2GRAY)
-# 	grayB = cv2.cvtColor(imageB, cv2.COLOR_BGR2GRAY)
+	# convert the images to grayscale
+	grayA = cv2.cvtColor(imageA, cv2.COLOR_BGR2GRAY)
+	grayB = cv2.cvtColor(imageB, cv2.COLOR_BGR2GRAY)
 
-# 	grayA = cv2.bilateralFilter(grayA,9,75,75)
-# 	grayB = cv2.bilateralFilter(grayB,9,75,75)
+	grayA = cv2.bilateralFilter(grayA,9,75,75)
+	grayB = cv2.bilateralFilter(grayB,9,75,75)
 
-# 	# cv2.imshow("grayB", grayB)
-# 	# cv2.imshow("im", grayB)
-# 	# cv2.waitKey(0)
+	# cv2.imshow("grayB", grayB)
+	# cv2.imshow("im", grayB)
+	# cv2.waitKey(0)
 
-# 	# compute the Structural Similarity Index (SSIM) between the two
-# 	# images, ensuring that the difference image is returned
-# 	(score, diff) = compare_ssim(grayA, grayB, full=True)
-# 	diff = (diff * 255).astype("uint8")
-# 	print("SSIM: {}".format(score))
+	# compute the Structural Similarity Index (SSIM) between the two
+	# images, ensuring that the difference image is returned
+	(score, diff) = compare_ssim(grayA, grayB, full=True)
+	diff = (diff * 255).astype("uint8")
+	print("SSIM: {}".format(score))
 
-# 	# threshold the difference image, followed by finding contours to
-# 	# obtain the regions of the two input images that differ
-# 	thresh = cv2.threshold(diff, 0, 255,
-# 		cv2.THRESH_BINARY_INV | cv2.THRESH_OTSU)[1]
-# 	cnts = cv2.findContours(thresh.copy(), cv2.RETR_EXTERNAL,
-# 		cv2.CHAIN_APPROX_SIMPLE)
-# 	cnts = imutils.grab_contours(cnts)
+	# threshold the difference image, followed by finding contours to
+	# obtain the regions of the two input images that differ
+	thresh = cv2.threshold(diff, 0, 255,
+		cv2.THRESH_BINARY_INV | cv2.THRESH_OTSU)[1]
+	cnts = cv2.findContours(thresh.copy(), cv2.RETR_EXTERNAL,
+		cv2.CHAIN_APPROX_SIMPLE)
+	cnts = imutils.grab_contours(cnts)
 
-# 	# loop over the contours
-# 	for c in cnts:
-# 		# compute the bounding box of the contour and then draw the
-# 		# bounding box on both input images to represent where the two
-# 		# images differ
-# 		(x, y, w, h) = cv2.boundingRect(c)
-# 		cv2.rectangle(imageA, (x, y), (x + w, y + h), (0, 0, 255), 2)
-# 		cv2.rectangle(imageB, (x, y), (x + w, y + h), (0, 0, 255), 2)
+	# loop over the contours
+	for c in cnts:
+		# compute the bounding box of the contour and then draw the
+		# bounding box on both input images to represent where the two
+		# images differ
+		(x, y, w, h) = cv2.boundingRect(c)
+		cv2.rectangle(imageA, (x, y), (x + w, y + h), (0, 0, 255), 2)
+		cv2.rectangle(imageB, (x, y), (x + w, y + h), (0, 0, 255), 2)
 	
-# 	cv2.imwrite("/home/tharunshiv/image-difference/one/"+str(i)+".jpg", imageA)     # save frame as JPG file
-# 	cv2.imwrite("/home/tharunshiv/image-difference/two/"+str(i)+"_2.jpg", imageB)     # save frame as JPG file
+	cv2.imwrite("/home/tharunshiv/image-difference/one/"+str(i)+".jpg", imageA)     # save frame as JPG file
+	cv2.imwrite("/home/tharunshiv/image-difference/two/"+str(i)+"_2.jpg", imageB)     # save frame as JPG file
 
-# 	# show the output images
-# 	# cv2.imshow("Original", imageA)
-# 	# cv2.imshow("Modified", imageB)
-# 	# cv2.imshow("Diff", diff)
-# 	# cv2.imshow("Thresh", thresh)
-# 	# cv2.waitKey(0)
+	# show the output images
+	# cv2.imshow("Original", imageA)
+	# cv2.imshow("Modified", imageB)
+	# cv2.imshow("Diff", diff)
+	# cv2.imshow("Thresh", thresh)
+	# cv2.waitKey(0)
 
-# #######
+#######
 
 
 import cv2
